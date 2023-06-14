4a65d113f06ac6e336fbb1d01de61bb71f191b08
kenanAST <kenanjoshua3454@gmail.com>
2023-06-14 21:16:40 +0800

    feat: added overlay with background feature

--- overlay.py
+++ overlay.py
@@ -1,4 +1,4 @@
-import cv2
+from PIL import Image
 import os
 
 
@@ -7,26 +7,35 @@ def overlay_images(folder_path, output_path, background_path):
     for filename in os.listdir(folder_path):
         if filename.endswith(".png") or filename.endswith(".jpg"):
             image_path = os.path.join(folder_path, filename)
-            image = cv2.imread(image_path, cv2.IMREAD_UNCHANGED)
+            image = Image.open(image_path)
             images.append(image)
 
-    if len(images) == 0:
+    if not images:
         print("No images found in the folder.")
         return
 
-    background = cv2.imread(background_path)
-    if background is None:
-        print("Background image not found.")
-        return
+    # Determine the dimensions of the final image
+    width = max(image.width for image in images)
+    height = max(image.height for image in images)
 
-    overlay = background
+    # Open the background image
+    background = Image.open(background_path)
+    background = background.resize((width, height))
+
+    # Create a new image with the background
+    merged_image = Image.new("RGBA", (width, height), (0, 0, 0, 0))
+    merged_image.paste(background, (0, 0))
+
+    # Overlay each image onto the merged image
     for image in images:
-        overlay = cv2.addWeighted(overlay, 1, image, 1, 0)
+        merged_image.paste(image, (0, 0), mask=image)
 
-    cv2.imwrite(output_path, overlay)
-    print("Overlay image saved at:", output_path)
+    # Save the merged image
+    merged_image.save(output_path)
+    print(f"Overlay image with background saved as {output_path}")
 
 
 folder_path = "./raw_data/drone_images"
+background_path = "./raw_data/sky_backgrounds/anime2.png"
 output_path = "./generated_dataset/1.png"
-overlay_images(folder_path, output_path)
+overlay_images(folder_path, output_path, background_path)
