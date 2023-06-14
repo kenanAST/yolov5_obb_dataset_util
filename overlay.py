68a133313fda82ea51c90ed4e19ce3f68ed8e7c7
kenanAST <kenanjoshua3454@gmail.com>
2023-06-14 23:02:30 +0800

    feat: added anchor_points feature

--- overlay.py
+++ overlay.py
@@ -1,11 +1,15 @@
 from PIL import Image
 import os
 
+anchor_points = [(102.4, 96),  (307.2, 96),  (512, 96),  (716.8, 96),  (921.6, 96),  (102.4, 288),  (307.2, 288),  (512, 288),  (716.8, 288),  (921.6, 288),
+                 (102.4, 480),  (307.2, 480),  (512, 480),  (716.8, 480),  (921.6, 480),  (102.4, 672),  (307.2, 672),  (512, 672),  (716.8, 672),  (921.6, 672)]
 
-def overlay_images(folder_path, output_path, background_path):
+
+def overlay_images(folder_path, background_path, output_path, rotations=None, anchor_points=None):
     images = []
     for filename in os.listdir(folder_path):
         if filename.endswith(".png") or filename.endswith(".jpg"):
+            print(filename)
             image_path = os.path.join(folder_path, filename)
             image = Image.open(image_path)
             images.append(image)
@@ -26,16 +30,26 @@ def overlay_images(folder_path, output_path, background_path):
     merged_image = Image.new("RGBA", (width, height), (0, 0, 0, 0))
     merged_image.paste(background, (0, 0))
 
-    # Overlay each image onto the merged image
-    for image in images:
-        merged_image.paste(image, (0, 0), mask=image)
+    # Overlay each image onto the merged image with rotation
+    for image, rotation, anchor_point in zip(images, rotations, anchor_points):
+        rotated_image = image.rotate(
+            rotation, expand=True, center=anchor_point)
+        merged_image.paste(rotated_image, (0, 0), mask=rotated_image)
 
     # Save the merged image
     merged_image.save(output_path)
-    print(f"Overlay image with background saved as {output_path}")
+    print(
+        f"Overlay image with background and individual rotations saved as {output_path}")
+
+# Example usage:
 
 
 folder_path = "./raw_data/drone_images"
 background_path = "./raw_data/sky_backgrounds/anime2.png"
 output_path = "./generated_dataset/1.png"
-overlay_images(folder_path, output_path, background_path)
+rotations = [0, 0, 0, 0, 0]  # Rotation angles for each image in degrees
+anchor_points = [(0, 0), (0, 0), (0, 0)
+                 ]  # Anchor points for each image
+
+overlay_images(folder_path, background_path,
+               output_path, rotations, anchor_points)
--- rotate.py
+++ rotate.py
@@ -1,35 +1,53 @@
-import numpy as np
+from PIL import Image
+import os
+
+anchor_points = [(102.4, 96),  (307.2, 96),  (512, 96),  (716.8, 96),  (921.6, 96),  (102.4, 288),  (307.2, 288),  (512, 288),  (716.8, 288),  (921.6, 288),
+                 (102.4, 480),  (307.2, 480),  (512, 480),  (716.8, 480),  (921.6, 480),  (102.4, 672),  (307.2, 672),  (512, 672),  (716.8, 672),  (921.6, 672)]
 
 
-def rotate_points(points, center, angle):
-    # Convert the angle to radians
-    theta = np.radians(angle)
+def overlay_images(folder_path, background_path, output_path, rotations=None, anchor_points=None):
+    images = []
+    for filename in os.listdir(folder_path):
+        if filename.endswith(".png") or filename.endswith(".jpg"):
+            image_path = os.path.join(folder_path, filename)
+            image = Image.open(image_path)
+            images.append(image)
 
-    # Create a rotation matrix
-    rotation_matrix = np.array([[np.cos(theta), -np.sin(theta)],
-                                [np.sin(theta), np.cos(theta)]])
+    if not images:
+        print("No images found in the folder.")
+        return
 
-    # Translate the points to the origin
-    translated_points = points - center
+    # Determine the dimensions of the final image
+    width = max(image.width for image in images)
+    height = max(image.height for image in images)
 
-    # Apply the rotation matrix to the translated points
-    rotated_points = np.dot(rotation_matrix, translated_points.T).T
+    # Open the background image
+    background = Image.open(background_path)
+    background = background.resize((width, height))
 
-    # Translate the points back to their original position
-    rotated_points += center
+    # Create a new image with the background
+    merged_image = Image.new("RGBA", (width, height), (0, 0, 0, 0))
+    merged_image.paste(background, (0, 0))
 
-    return rotated_points
+    # Overlay each image onto the merged image with rotation
+    for image, rotation, anchor_point in zip(images, rotations, anchor_points):
+        rotated_image = image.rotate(
+            rotation, expand=True, center=anchor_point)
+        merged_image.paste(rotated_image, (0, 0), mask=rotated_image)
+
+    # Save the merged image
+    merged_image.save(output_path)
+    print(
+        f"Overlay image with background and individual rotations saved as {output_path}")
 
 
-# Original points
-points = np.array([[100, 100], [200, 100], [200, 200],
-                  [100, 200]], dtype=np.float32)
+# Example usage:
+folder_path = "path/to/folder"
+background_path = "path/to/background.jpg"
+output_path = "path/to/output/overlay_image_with_background_rotations_anchor.png"
+rotations = [0, 45, -30]  # Rotation angles for each image in degrees
+anchor_points = [(100, 100), (200, 200), (300, 300)
+                 ]  # Anchor points for each image
 
-# Center of rotation (assumed to be the center of the image)
-center = np.array([150, 150], dtype=np.float32)
-
-# Angle of rotation in degrees
-angle = 45
-
-# Rotate the points
-rotated_points = rotate_points(points, center, 0 + 5)
+overlay_images(folder_path, background_path,
+               output_path, rotations, anchor_points)
