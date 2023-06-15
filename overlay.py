1d2a907dce51cb17f94fcbf7af65dfe94199ad1d
kenanAST <kenanjoshua3454@gmail.com>
2023-06-15 12:20:27 +0800

    fix: removed buggy rotation

--- overlay.py
+++ overlay.py
@@ -1,14 +1,5 @@
-from PIL import Image
+from PIL import Image, ImageDraw
 import os
-
-# A 4x5 grid division of a (1024, 768) image.
-anchor_points = [
-    [(128, 77), (384, 77), (640, 77), (896, 77)],
-    [(128, 231), (384, 231), (640, 231), (896, 231)],
-    [(128, 385), (384, 385), (640, 385), (896, 385)],
-    [(128, 539), (384, 539), (640, 539), (896, 539)],
-    [(128, 693), (384, 693), (640, 693), (896, 693)],
-]
 
 
 def overlay_images(folder_path, background_path, output_path, rotations=None, anchor_points=None):
@@ -36,26 +27,27 @@ def overlay_images(folder_path, background_path, output_path, rotations=None, an
     merged_image = Image.new("RGBA", (width, height), (0, 0, 0, 0))
     merged_image.paste(background, (0, 0))
 
-    # Overlay each image onto the merged image with rotation
-    for image, rotation, anchor_point in zip(images, rotations, anchor_points):
-        rotated_image = image.rotate(
-            rotation, expand=True, center=anchor_point)
-        merged_image.paste(rotated_image, (0, 0), mask=rotated_image)
+    # Overlay each image onto the merged image
+    for image in images:
+        merged_image.paste(image, (0, 0), mask=image)
 
     # Save the merged image
     merged_image.save(output_path)
     print(
         f"Overlay image with background and individual rotations saved as {output_path}")
 
-# Example usage:
-
 
 folder_path = "./raw_data/drone_images"
 background_path = "./raw_data/sky_backgrounds/anime2.png"
 output_path = "./generated_dataset/1.png"
-rotations = [0, 0, 0, 0, 0]  # Rotation angles for each image in degrees
-anchor_points = [(0, 0), (0, 0), (0, 0)
-                 ]  # Anchor points for each image
+rotations = [10, 90, 0, 0, 0]  # Rotation angles for each image in degrees
+anchor_points = [
+    (896, 693),  # Anchor point for the first image
+    (896, 539),  # Anchor point for the second image
+    (896, 539),  # Anchor point for the third image
+    (640, 77),   # Anchor point for the fourth image
+    (896, 77)    # Anchor point for the fifth image
+]
 
 overlay_images(folder_path, background_path,
                output_path, rotations, anchor_points)
